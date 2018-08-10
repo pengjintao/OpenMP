c42afa4b165200d9d95b2a71c66fb8833343013d
pengjintao <1272813056@qq.com>
2018-08-10 16:04:29 +0800

    控制线程数量-读取线程id和线程数量-openmpq安全检查

--- 01_num_threads.cpp
+++ 01_num_threads.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 #include <time.h>
-#include <omp.h>
 #include <unistd.h>
+
+#ifdef _OPENMP
+#include <omp.h>
+#endif
+
 
 
 void Hello(void);
@@ -10,7 +14,7 @@ using namespace std;
 int main()
 {
 	int thread_count = 4000;
-
+#if
 	#pragma omp parallel num_threads(thread_count)
 	Hello();
 
@@ -19,8 +23,14 @@ int main()
 
 void Hello()
 {
+#ifdef _OPENMP
 	int my_rank = omp_get_thread_num();
 	int thread_count = omp_get_num_threads();
+# else
+	int my_rank = 0;
+	int thread_count = 1;
+
+#endif
 	int a = 0;
 	for(int i=0;i<thread_count;i++)
 	{
