4c4c7d8abcd623e484bc946e9ff3da2b7d4255e9
pengjintao <1272813056@qq.com>
2018-11-13 22:19:34 +0800

    'optimization'

--- 08_odd_even_sort_faster.cpp
+++ 08_odd_even_sort_faster.cpp
@@ -22,14 +22,15 @@ int main()
 	int thread_count = 8;
 	int i = 0;
 	int tmp = 0;
-
+	int phase;
 	clock_gettime(CLOCK_PROCESS_CPUTIME_ID,&start);
-	for(int phase = 0;phase < size;phase++)
+	#pragma omp parallel  num_threads(thread_count)\
+			default(none) shared(vec,size) private(i,tmp,phase)
+	for(phase = 0;phase < size;phase++)
 	{
 		if(phase %2 ==0)
 		{
-			#pragma omp parallel for num_threads(thread_count)\
-			default(none) shared(vec,size) private(i,tmp)
+			#pragma omp for
 			for(i = 1;i<size;i+=2)
 			{
 				if(vec[i-1] > vec[i]){
@@ -39,8 +40,7 @@ int main()
 				}
 			}
 		}else{
-			#pragma omp parallel for num_threads(thread_count)\
-			default(none) shared(vec,size) private(i,tmp)
+			#pragma omp for
 			for(i = 1;i<size-1;i+=2)
 			{
 				if(vec[i] > vec[i+1])
